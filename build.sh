c0182aaa772b5bba2f8f37e5e20ca41c02276a12
FrankRL12 <ramirezlujanojosefrancisco4@gmail.com>
2023-05-22 21:26:32 -0600

    commit

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ python manage.py makemigrations
 python manage.py migrate
 
 # Variables de entorno para el superusuario
-SUPERUSER_USERNAME="admin2"
+SUPERUSER_USERNAME="admin3"
 SUPERUSER_EMAIL="fermenger05@gmail.com"
 SUPERUSER_PASSWORD="+t@ckc&11"
 
